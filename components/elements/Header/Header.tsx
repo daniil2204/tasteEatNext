244cc13cff0ff825bccc54ce4a4d386e1cd23a67
daniil2204 <dragindaniilmeizu@gmail.com>
2023-12-22 12:19:20 +0200

    logoFix

--- components/elements/Header/Header.tsx
+++ components/elements/Header/Header.tsx
@@ -1,19 +1,12 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styles from './Header.module.scss'
 import Image from 'next/image'
-import { useWindowWidth } from '@/hooks/useMediaQuery'
-import { sizeInterface } from '@/types/header'
+
 import Navigation from './elements/navigation/Navigation'
-import { sizeLogoByWidth } from '@/utils/sizeLogoByWidth'
 
 const Header = () => {
-  const { windowWidth } = useWindowWidth()
-  const [size, setSize] = useState<sizeInterface>({ width: 378, height: 188 })
-  useEffect(() => {
-    const logoSize = sizeLogoByWidth(windowWidth.innerWidth - 70)
-    setSize(logoSize)
-  }, [windowWidth])
+  console.log('hello')
   return (
     <header className={styles.header}>
       <div className={styles.banner}>
@@ -27,10 +20,11 @@ const Header = () => {
           />
         </button>
         <Image
+          className={styles.logo}
           alt="restaurant logo"
           src="/assets/img/bannerLogo.svg"
-          width={size.width}
-          height={size.height}
+          width={378}
+          height={188}
         />
         <button className={`${styles.btn} ${styles.btn__reservation}`}>
           <p>Reservation</p>
